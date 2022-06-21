5838a5d3e097e01deea6e3bd6cee78be9bb275aa
idelioofico <oficoidelio@gmail.com>
2022-06-21 23:25:17 +0200

    Renew api's

--- app/Http/Controllers/api/v1/EventControllerApi.php
+++ app/Http/Controllers/api/v1/EventControllerApi.php
@@ -12,7 +12,7 @@ class EventControllerApi extends Controller{
 
         public function categories()
         {
-                $vent_types=implode(EventType::get()->pluck('name'));
+                $vent_types=strtolower(implode(", ",EventType::get()->pluck('name')->toArray()));
 
                 return response()->json(['data'=>$vent_types],200);
         }
