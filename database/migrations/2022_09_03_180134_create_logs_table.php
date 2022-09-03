a89998e246f8fb8c55ac60503e5e2acf2c93356a
idelioofico <oficoidelio@gmail.com>
2022-09-03 21:04:56 +0200

    Event api

--- database/migrations/2022_09_03_180134_create_logs_table.php
+++ database/migrations/2022_09_03_180134_create_logs_table.php
@@ -16,6 +16,7 @@ class CreateLogsTable extends Migration
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
             $table->string("action")->nullable();
+            $table->string('status')->nullable();
             $table->text("request")->nullable();
             $table->text("response")->nullable();
             $table->string('ip')->nullable();
